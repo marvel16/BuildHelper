c914501f9525535eafc64ed92fd873cf4c27a2d8
marvel16 <marvelwall@gmail.com>
2014-12-20 10:58:23 +0200

    Async writing to output

--- BuildHelper/MainWindow.xaml.cs
+++ BuildHelper/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Xml.Serialization;
 using System.IO;
 using MahApps.Metro.Controls;
 using System.Net;
+using System.ComponentModel;
 using Microsoft.TeamFoundation;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
@@ -32,100 +33,116 @@ namespace BuildHelper
 	public partial class MainWindow : MetroWindow
 	{
 		private bool bBuildsLaunched = false;
-		private List<Project> Config = new List<Project>();
-		private TFSAccount tfscfg = new TFSAccount();
+		private CfgMan config = new CfgMan();
+		private List<CheckBox> checkboxes = new List<CheckBox>();
+		private List<Process> ProcessPool = new List<Process>();
 		
 		public MainWindow( )
 		{
 			InitializeComponent();
-			CfgMan.LoadConfig(ref Config);
-			CfgMan.LoadConfig(ref tfscfg);
+			
+			config.LoadConfig();
 
-			foreach ( var item in Config )
+			foreach ( var item in config.Prjcfg )
 				ProjectListBox.Items.Add(item);
-			tfs_username_textbox.Text = tfscfg.UserName;
-			tfs_path_textbox.Text = tfscfg.TfsPath;
-			tfs_workspace_textbox.Text = tfscfg.TfsWorkspace;
-			requestpath_textbox.Text = tfscfg.RequestPath;
+			tfs_username_textbox.Text = config.Tfscfg.UserName;
+			tfs_path_textbox.Text = config.Tfscfg.TfsPath;
+			tfs_workspace_textbox.Text = config.Tfscfg.TfsWorkspace;
+			requestpath_textbox.Text = config.Tfscfg.RequestPath;
 		}
 
 		private void LaunchButton_OnClick(object sender, EventArgs e)
 		{
-			if(Config.Count == 0)
+			if(config.Prjcfg.Count == 0)
 			{
 				MessageBox.Show("Config is empty");
 				return;
 			}
 			status_progressRing.IsActive = !status_progressRing.IsActive;
-			if ( bBuildsLaunched )
+			if (bBuildsLaunched)
 			{
-				//TODO: cancel builds
+				foreach ( Process proc in ProcessPool )
+					proc.Close();
+				ProcessPool.Clear();
+				output_listbox.Items.Add("BUILDS CANCELLED!");
 				Launch.Content = "Launch builds!";
 				bBuildsLaunched = !bBuildsLaunched;
 				return;
 			}
-			//TODO: launch builds
-			List<Process> ProcessPool = new List<Process>();
-			for ( int i=0; i < Config.Count; i++ )
+
+			StartBuilds(); //launch devenv process
+			output_listbox.Items.Add("BUILDS LAUNCHED!");
+			bBuildsLaunched = true;
+			Launch.Content = "Cancel builds";
+			Task.Run(() => WriteOutput());
+			
+			
+		}
+
+		private void WriteOutput()
+		{
+			string result = ProcessPool[0].StandardOutput.ReadLine();
+			while ( result != null )
 			{
-				string rebuildInfo = ParseConfig(Config[i]);
+				output_listbox.Dispatcher.Invoke((Action)( ( ) => output_listbox.Items.Add(result) ));
+				result = ProcessPool[0].StandardOutput.ReadLine();
+			}
+		}
+
+		private void StartBuilds()
+		{
+			for ( int i=0; i < config.Prjcfg.Count; i++ )
+			{
+				string rebuildInfo = ParseConfig(config.Prjcfg[i]);
 				ProcessStartInfo start = new ProcessStartInfo();
 				start.FileName = "C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/devenv.com";
 				start.UseShellExecute = false;
 				start.RedirectStandardOutput = true;
-				start.Arguments = Config[i].ProjectPath + @" /REBUILD " + rebuildInfo;
+				start.Arguments = config.Prjcfg[i].ProjectPath + @" /REBUILD " + rebuildInfo;
 				ProcessPool.Add(Process.Start(start));
 			}
-			string result;
-			while ( ( result = ProcessPool[0].StandardOutput.ReadLine() ) != null )
-				output_listbox.Items.Add(result);
-
-				bBuildsLaunched = true;
-			Launch.Content = "Cancel builds";
 		}
-
-		
 
 		private void x64R_checkbox_CheckedChange( object sender, RoutedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			Config[ProjectListBox.SelectedIndex].isChecked_x64R = (bool)( (CheckBox)sender ).IsChecked;
-			Config.SaveConfig();
+			config.Prjcfg[ProjectListBox.SelectedIndex].x64R = (bool)( (CheckBox)sender ).IsChecked;
+			config.SaveConfig();
 		}
 
 		private void x64D_checkbox_CheckedChange( object sender, RoutedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			Config[ProjectListBox.SelectedIndex].isChecked_x64D = (bool)( (CheckBox)sender ).IsChecked;
-			Config.SaveConfig();
+			config.Prjcfg[ProjectListBox.SelectedIndex].x64D = (bool)( (CheckBox)sender ).IsChecked;
+			config.SaveConfig();
 		}
 
 		private void x86R_checkbox_CheckedChange( object sender, RoutedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			Config[ProjectListBox.SelectedIndex].isChecked_x86R = (bool)( (CheckBox)sender ).IsChecked;
-			Config.SaveConfig();
+			config.Prjcfg[ProjectListBox.SelectedIndex].x86R = (bool)( (CheckBox)sender ).IsChecked;
+			config.SaveConfig();
 		}
 
 		private void x86D_checkbox_CheckedChange( object sender, RoutedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			Config[ProjectListBox.SelectedIndex].isChecked_x86D = (bool)( (CheckBox)sender ).IsChecked;
-			Config.SaveConfig();
+			config.Prjcfg[ProjectListBox.SelectedIndex].x86D = (bool)( (CheckBox)sender ).IsChecked;
+			config.SaveConfig();
 		}
 
 		private void ProjectListBox_SelectionChanged( object sender, SelectionChangedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			x64D_checkbox1.IsChecked = Config[ProjectListBox.SelectedIndex].isChecked_x64D;
-			x64R_checkbox1.IsChecked = Config[ProjectListBox.SelectedIndex].isChecked_x64R;
-			x86R_checkbox1.IsChecked = Config[ProjectListBox.SelectedIndex].isChecked_x86R;
-			x86D_checkbox1.IsChecked = Config[ProjectListBox.SelectedIndex].isChecked_x86D;
+			x64D_checkbox1.IsChecked = config.Prjcfg[ProjectListBox.SelectedIndex].x64D;
+			x64R_checkbox1.IsChecked = config.Prjcfg[ProjectListBox.SelectedIndex].x64R;
+			x86R_checkbox1.IsChecked = config.Prjcfg[ProjectListBox.SelectedIndex].x86R;
+			x86D_checkbox1.IsChecked = config.Prjcfg[ProjectListBox.SelectedIndex].x86D;
 		}
 
 		private void createProject_button_Click( object sender, RoutedEventArgs e )
@@ -145,19 +162,19 @@ namespace BuildHelper
 			proj.ProjectName = Projectname_textbox.Text;
 			proj.ProjectPath = Projectpath_textbox.Text;
 			ProjectListBox.Items.Add(proj);
-			Config.Add(proj);
-			Config.SaveConfig();
+			config.Prjcfg.Add(proj);
+			config.SaveConfig();
 		}
 
 		private void removeproject_button_Click( object sender, RoutedEventArgs e )
 		{
 			if ( ProjectListBox.SelectedIndex < 0 )
 				return;
-			Config.RemoveAt(ProjectListBox.SelectedIndex);
+			config.Prjcfg.RemoveAt(ProjectListBox.SelectedIndex);
 			ProjectListBox.Items.Clear();
-			foreach ( var item in Config )
+			foreach ( var item in config.Prjcfg )
 				ProjectListBox.Items.Add(item);
-			Config.SaveConfig();
+			config.SaveConfig();
 		}
 
 		private void Projectpath_textbox_TextChanged( object sender, TextChangedEventArgs e )
@@ -171,13 +188,13 @@ namespace BuildHelper
 		private string ParseConfig(Project sol)
 		{
 			string ret = string.Empty;
-			if ( sol.isChecked_x64D )
+			if ( sol.x64D )
 				ret = @"Debug|x64";
-			if ( sol.isChecked_x86D )
+			if ( sol.x86D )
 				ret = @"Debug|x86";
-			if ( sol.isChecked_x64R )
+			if ( sol.x64R )
 				ret = @"Release|x64";
-			if ( sol.isChecked_x86R )
+			if ( sol.x86R )
 				ret = @"Release|x86";
 			return ret;
 		}
@@ -187,26 +204,19 @@ namespace BuildHelper
 			FetchCode();
 		}
 
-		private void FetchCode()
+		private void FetchCode( )
 		{
-			try
-			{
-				string userName = tfs_username_textbox.Text;
-				string userPass = pw_passwordbox.Password;
-				string tfsPath = tfs_path_textbox.Text;
-				string tfsWorkSpace = tfs_workspace_textbox.Text;
-				string requestPath = requestpath_textbox.Text;
-				ICredentials myCred = new NetworkCredential(userName, userPass);
-				TeamFoundationServer tfs = new TeamFoundationServer(tfsPath, myCred);
-				VersionControlServer vcs = tfs.GetService<VersionControlServer>();
-				Workspace myWorkspace = vcs.GetWorkspace(tfsWorkSpace, vcs.AuthorizedUser);
-				GetRequest request = new GetRequest(new ItemSpec(requestPath, RecursionType.Full), VersionSpec.Latest);
-				GetStatus getStat = myWorkspace.Get(request, GetOptions.None);
-			}
-			catch(Exception ex)
-			{
-				MessageBox.Show("Error during fetching code: " + ex.Message);
-			}
+			string userName = tfs_username_textbox.Text;
+			string userPass = pw_passwordbox.Password;
+			string tfsPath = tfs_path_textbox.Text;
+			string tfsWorkSpace = tfs_workspace_textbox.Text;
+			string requestPath = requestpath_textbox.Text;
+			ICredentials myCred = new NetworkCredential(userName, userPass);
+			TeamFoundationServer tfs = new TeamFoundationServer(tfsPath, myCred);
+			VersionControlServer vcs = tfs.GetService<VersionControlServer>();
+			Workspace myWorkspace = vcs.GetWorkspace(tfsWorkSpace, vcs.AuthorizedUser);
+			GetRequest request = new GetRequest(new ItemSpec(requestPath, RecursionType.Full), VersionSpec.Latest);
+			GetStatus getStat = myWorkspace.Get(request, GetOptions.None);
 		}
 
 		private void FetchCheckBox_Click( object sender, RoutedEventArgs e )
@@ -217,13 +227,11 @@ namespace BuildHelper
 
 		private void rememberTFScfg_click( object sender, RoutedEventArgs e )
 		{
-			if ( tfscfg == null )
-				tfscfg = new TFSAccount();
-			tfscfg.UserName = tfs_username_textbox.Text;
-			tfscfg.TfsPath = tfs_path_textbox.Text;
-			tfscfg.TfsWorkspace = tfs_workspace_textbox.Text;
-			tfscfg.RequestPath = requestpath_textbox.Text;
-			CfgMan.SaveConfig(tfscfg);
+			config.Tfscfg.UserName = tfs_username_textbox.Text;
+			config.Tfscfg.TfsPath = tfs_path_textbox.Text;
+			config.Tfscfg.TfsWorkspace = tfs_workspace_textbox.Text;
+			config.Tfscfg.RequestPath = requestpath_textbox.Text;
+			config.SaveConfig();
 		}
 
 			
@@ -252,68 +260,81 @@ namespace BuildHelper
 	{
 		public string ProjectName = String.Empty;
 		public string ProjectPath = String.Empty;
-		public VCS projectVCS = VCS.TFS;
-		public bool isChecked_x86D = false;
-		public bool isChecked_x86R = false;
-		public bool isChecked_x64D = false;
-		public bool isChecked_x64R = false;
+		public VCS projectVCS = VCS.TFS; //TODO
+		public bool x86D = false;
+		public bool x86R = false;
+		public bool x64D = false;
+		public bool x64R = false;
 		public override string ToString( )
 		{
 			return ProjectName;
 		}
+		public byte GetBitFieldConfig()
+		{
+			byte field = 0;
+			if ( x86D )
+				field |= 1;
+			if ( x86R )
+				field |= 1 << 1;
+			if ( x64D )
+				field |= 1 << 2;
+			if ( x64R )
+				field |= 1 <<3;
+			return field;
+		}
 	}
 
-	public static class CfgMan
+	public class CfgMan
 	{
+		public List<Project> Prjcfg = new List<Project>();
+		public TFSAccount Tfscfg = new TFSAccount();
 		private static string currentDir = Directory.GetCurrentDirectory();
-		public static void SaveConfig(this object obj)
-		{
-			string cfgname = String.Empty;
-			Type type = obj.GetType();
 
-			if ( type == typeof(List<Project>) )
-				cfgname = "config.xml";
-			else
-				cfgname = "tfsconfig.xml";
-			using ( FileStream fs = new FileStream(currentDir + @"\" + cfgname, FileMode.Create) )
+		public void SaveConfig()
+		{
+			Serialize(Prjcfg, "config.xml");
+			Serialize(Tfscfg, "tfsconfig.xml");
+		}
+
+		public void LoadConfig()
+		{
+			Deserialize(ref Prjcfg, "config.xml");
+			Deserialize(ref Tfscfg, "tfsconfig.xml");
+		}
+
+		private void Serialize<T>(T cfg, string path)
+		{
+			try
 			{
-				XmlWriter writer = XmlWriter.Create(fs);
-				XmlSerializer ser = new XmlSerializer(type);
-				ser.Serialize(writer, obj);
+				using ( FileStream fs = new FileStream(currentDir + @"\" + path, FileMode.Create) )
+				{
+					XmlWriter writer = XmlWriter.Create(fs);
+					XmlSerializer ser = new XmlSerializer(typeof(T));
+					ser.Serialize(writer, cfg);
+				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show("Error during saving config: " + ex.Message);
 			}
 		}
-		public static void LoadConfig(ref List<Project> cfg )
+		private void Deserialize<T>(ref T cfg, string path)
 		{
-			string cfgname = "config.xml";
-			if ( !File.Exists(currentDir + @"\" + cfgname) )
+			if ( !File.Exists(currentDir + @"\" + path) )
 				return;
 			try
 			{
-				using ( FileStream fs = new FileStream(currentDir + @"\" + cfgname, FileMode.OpenOrCreate) )
+				using ( FileStream fs = new FileStream(currentDir + @"\" + path, FileMode.OpenOrCreate) )
 				{
 					XmlReader reader = XmlReader.Create(fs);
-					XmlSerializer ser = new XmlSerializer(typeof(List<Project>));
-					cfg = (List<Project>)ser.Deserialize(reader);
+					XmlSerializer ser = new XmlSerializer(typeof(T));
+					cfg = (T)ser.Deserialize(reader);
 				}
 			}
-			catch ( Exception ex ) { }
-		}
-		public static void LoadConfig(ref TFSAccount cfg)
-		{
-			string cfgname = "tfsconfig.xml";
-			if ( !File.Exists(currentDir + @"\" + cfgname) )
-				return;
-			try
+			catch ( Exception ex )
 			{
-				using ( FileStream fs = new FileStream(currentDir + @"\" + cfgname, FileMode.OpenOrCreate) )
-				{
-					XmlReader reader = XmlReader.Create(fs);
-					XmlSerializer ser = new XmlSerializer(typeof(TFSAccount));
-					cfg = (TFSAccount)ser.Deserialize(reader);
-				}
+				MessageBox.Show("Error during loading config: " + ex.Message);
 			}
-			catch ( Exception ex ) { }
-
 		}
 	}
 }
