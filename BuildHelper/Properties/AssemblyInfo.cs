dd7b71f2aa83fc1ede2e91f8551aa133091d373a
marvel16 <marvelwall@gmail.com>
2015-01-12 01:17:33 +0200

    Icon

--- BuildHelper/Properties/AssemblyInfo.cs
+++ BuildHelper/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildHelper")]
-[assembly: AssemblyCopyright("MZ & Marvel Copyright ©  2014")]
+[assembly: AssemblyCopyright("Marvel Copyright ©  2014")]
 [assembly: AssemblyTrademark("All rights reserved, bitch!")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
